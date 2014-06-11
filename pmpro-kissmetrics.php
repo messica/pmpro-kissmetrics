8a99a348416ee97af53607543493363d133fa122
Jessica Oros <orosjess@gmail.com>
2014-06-11 18:33:33 +0000

    Initial Commit

---
+++ pmpro-kissmetrics.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: PMPro KISSmetrics
+ * Description: Integrates PMPro with KISSmetrics to track user activity.
+ * Author: Stranger Studios
+ * Author URI: http://strangerstudios.com
+ * Version: .1
+ */
